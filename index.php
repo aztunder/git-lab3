7992e1d9ce8f1e88679d71def1d63b83f091ba5d
aztunder <160449896+aztunder@users.noreply.github.com>
2025-07-17 16:33:50 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?
-    echo"this subject is git tutorial"
-    echo"this subject is git tutorial"
+    echo"this subject is git tutorial";
+    echo"this subject is git tutorial";
 
-    echo"add file เพิ่มเติมแล้วครับ"
-    echo"แก้บัคerror เรียบร้อย"
+    echo"add file เพิ่มเติมแล้วครับ";
+    echo"แก้บัคerror เรียบร้อย";
 ?>
